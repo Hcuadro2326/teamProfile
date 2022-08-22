8f5f4cae3ad798f6432fbb4b2a8acf8d91f5e463
Hector Cuadro <105142275+Hcuadro2326@users.noreply.github.com>
2022-08-22 11:08:03 -0500

    Update Manager.test.js

--- Desktop/teamProfile/__tests__/Manager.test.js
+++ Desktop/teamProfile/__tests__/Manager.test.js
@@ -3,14 +3,14 @@ const Manager = require('../lib/Manager');
 
 // creating manager object  
 test('creates an Manager object', () => {
-    const manager = new Manager('Nicole', 90, 'nicole.elisaw@gmail', 4);
+    const manager = new Manager('Hector', 25 ,'cuadro.bebo2326@gmail.com', 2);
     
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // gets role from getRole()
 test('gets role of employee', () => {
-    const manager = new Manager('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const manager = new Manager('Hector', 25 ,'cuadro.bebo2326@gmail.com');
 
     expect(manager.getRole()).toEqual("Manager");
 }); 
