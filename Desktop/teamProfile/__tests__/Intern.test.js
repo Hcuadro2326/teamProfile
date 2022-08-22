be87d650c0d7c9d8ecff56e2d1abd2873555ef14
Hector Cuadro <105142275+Hcuadro2326@users.noreply.github.com>
2022-08-22 11:07:34 -0500

    Update Intern.test.js

--- Desktop/teamProfile/__tests__/Intern.test.js
+++ Desktop/teamProfile/__tests__/Intern.test.js
@@ -3,21 +3,21 @@ const Intern = require('../lib/Intern');
 
 // creating intern object  
 test('creates an Intern object', () => {
-    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail', 'SFSU');
+    const intern = new Intern('Hector', 25 ,'cuadro.bebo2326@gmail.com', 'SMU');
     
     expect(intern.school) .toEqual(expect.any(String));
 });
 
 // gets school from getSchool()
 test('gets employee school', () => {
-    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail', 'SFSU');
+    const intern = new Intern('Hector', 25 ,'cuadro.bebo2326@gmail.com', 'SMU');
     
     expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
 });
 
 // gets role from getRole()
 test('gets role of employee', () => {
-    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail.com', 'SFSU');
+    const intern = new Intern('Hector', 25 ,'cuadro.bebo2326@gmail.com', 'SMU');
 
     expect(intern.getRole()).toEqual("Intern");
 }); 
